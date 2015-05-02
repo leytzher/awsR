0516949c4da73efe121fdcd98ab04bda6315580f
leytzher <leytzher@gmail.com>
2015-05-02 15:13:11 +0800

    Load URL

--- AWSQuery.R
+++ AWSQuery.R
@@ -1,13 +1,14 @@
 # Import libraries
 library(rPython)
 library(XML)
+library(RCurl)
 
 
 #Set working directory
 setwd("~/Documents/asm/R")
 
 #Set keywords: use "+" for space
-keywords<-"Horse"
+keywords<-"Bali"
 write.csv(keywords,file="keywords.csv",row.names=FALSE,col.names=NA)
 
 # Load the main Python script
@@ -15,5 +16,12 @@ python.load("/Users/user/Documents/asm/R/AWS.py")
 myURL<-python.get("myURL")
 
 # Load XML file
+query= getURL(myURL)
+
+# Parse XML file
+xmlfile<-xmlTreeParse(query)
+
+# use xmlRoot to access the top node
+xmltop = xmlRoot(xmlfile)
 
 
